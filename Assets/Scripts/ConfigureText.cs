72334d07bd4099f84c28dba41aac4c9b11e4a75c
5665tm <5665tm@gmail.com>
2016-04-18 19:58:07 +0300

    initial commit

---
+++ Assets/Scripts/ConfigureText.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigureText : MonoBehaviour
+{
+	private void Start()
+	{
+		GetComponent<Text>().text = transform.parent.name;
+	}
+}
---
+++ Assets/Scripts/Context.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Context : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _troll;
+
+	public void SwitchAnimation(string animationName)
+	{
+		_troll.GetComponent<Animation>().Play(animationName);
+	}
+}
